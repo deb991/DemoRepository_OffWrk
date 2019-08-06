5358070e7d11ab2c27de242f6ab86b80d30ff57a
Ozzius <deb991@users.noreply.github.com>
2019-08-06 09:51:27 +0530

    URL Shorten using URLLIB
    URL Shorten using URLLIB

---
+++ urlshortner/urlshort.py
@@ -0,0 +1,28 @@
+from __future__ import with_statement
+import contextlib
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+import sys
+
+
+def make_tiny(url):
+    request_url = ('http://tinyurl.com/api-create.php?' +
+    urlencode({'url':url}))
+    with contextlib.closing(urlopen(request_url)) as response:
+        return response.read().decode('utf-8')
+
+def main():
+    urls = sys.argv[1:]
+    for url in urls:
+        tinyurl = make_tiny(url)
+        print(tinyurl)
+
+if __name__ == '__main__':
+    main()
+
